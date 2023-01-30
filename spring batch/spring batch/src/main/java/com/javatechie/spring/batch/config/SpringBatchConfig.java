64d711b98a06fbb775cfb1c7a1529bad6e5f52bc
Riya1356 <108179852+Riya1356@users.noreply.github.com>
2023-01-29 18:07:09 -0800

    Add files via upload

---
+++ spring batch/spring batch/src/main/java/com/javatechie/spring/batch/config/SpringBatchConfig.java
@@ -0,0 +1,106 @@
+package com.javatechie.spring.batch.config;
+
+import com.javatechie.spring.batch.entity.Customer;
+import com.javatechie.spring.batch.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+
+    private StepBuilderFactory stepBuilderFactory;
+
+    private CustomerRepository customerRepository;
+
+    // Making ItemReader
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+
+    // Giving instructions to map our data from file to the customer table in DB
+    private LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+
+    }
+
+    // Making a custom processor to process the data
+    @Bean
+    public CustomerProcessor processor() {
+        return new CustomerProcessor();
+    }
+
+
+    //Making a writer that will finally write the formatted data into the db via repository
+    @Bean
+    public RepositoryItemWriter<Customer> writer() {
+        RepositoryItemWriter<Customer> writer = new RepositoryItemWriter<>();
+        writer.setRepository(customerRepository);
+        writer.setMethodName("save");
+        return writer;
+    }
+
+    // once 1-> ItemReader, 2-> ItemProcesser, 3-> ItemWriter are done then we build step using all these.
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("csv-step").<Customer, Customer>chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    // Once step is built, then we build the job using steps (if multiple). Here we have only 1, so we are using only 1.
+    @Bean
+    public Job runJob() {
+        return jobBuilderFactory.get("importCustomers")
+                .flow(step1()).end().build();
+    }
+
+   // this is used to make threads asynchronous so that concurrent execution can happen. here 10 is the number of threads
+    // that we want to create.
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
+    }
+
+}
